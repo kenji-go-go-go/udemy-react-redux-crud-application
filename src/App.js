754dfd54edd170f422653b201f30f20b8d5b806c
kenji-go-go-go <FZF01113@nifty.com>
2023-08-25 23:49:00 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
 
-const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "NoName", age: 3 },
-  ];
-  return (
-    <div>
-      {profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />;
-      })}
-    </div>
-  );
-};
+const App = () => <Counter></Counter>;
 
-const User = (props) => {
-  return (
-    <div>
-      Hi, I am {props.name}, and {props.age}
-    </div>
-  );
-};
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired,
-};
+  hondlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  hondleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count:{this.state.count}</div>
+        <button onClick={this.hondlePlusButton}>+1</button>
+        <button onClick={this.hondleMinusButton}>-1</button>
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
