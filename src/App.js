82d0b9c82879104d3c58db68a25de35c4ca2d6a5
kenji-go-go-go <FZF01113@nifty.com>
2023-08-25 20:57:24 +0900

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React,{ Component } from "react";
 
-
-function App() {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const greeting = "Welcome to React";
+    const dom = <h1 className="foo">{greeting}</h1>;
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => console.log("clicked")} />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
